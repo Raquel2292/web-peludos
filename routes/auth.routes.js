2f3c6cff4be939ae3d40e2762290dbfe067b75e2
johndoe@example.com <johndoe@example.com>
2022-10-21 18:27:47 +0100

    Rutas Done

---
+++ routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// Rutas de autenticación
+
+
+
+
+
+
+
+module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,20 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+
+const authRoutes = require("./auth.routes");
+router.use("/auth", authRoutes);
+
+const profileRoutes = require("./profile.routes");
+router.use("/profile", profileRoutes);
+
+const commentsRoutes = require("./comments.routes");
+router.use("/comments", commentsRoutes);
+
+const userAdminRoutes = require("./user-admin.routes");
+router.use("/user-admin", userAdminRoutes);
+
+const productsRoutes = require("./products.routes");
+router.use("/products", productsRoutes);
+
 module.exports = router;
