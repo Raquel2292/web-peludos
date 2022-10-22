88a40bb34b3b1e4c00bc1b392780f4724f14cc25
johndoe@example.com <johndoe@example.com>
2022-10-22 11:07:31 +0100

    Formularios de Registro

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,8 +3,32 @@ const router = express.Router();
 
 // Rutas de autenticación
 
+//1. GET renderizar vista de registro
+
+router.get("/signup", (req, res, next) =>{
+    res.render("auth/signup.hbs")
+
+})
+
+//2. POST recibir la información del formulrio
+
+router.post("/signup", (req, res, next) =>{
 
 
+
+
+
+    res.redirect("/")
+
+})
+
+
+//3. GET renderizar vista de formulario de acceso a la pagina
+router.get("/login", (req, res, next) =>{
+    res.render("auth/login.hbs")
+
+})
+//4. POST recibe credenciales del usuario y valida
 
 
 
