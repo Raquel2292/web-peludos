8935b2c99f67316bb4f74b75f71610625af6c244
Raquel Blázquez <raquel.blazquez.estrada@gmail.com>
2022-10-24 21:30:51 +0200

    perfil hecho, elimina y edita

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -5,11 +5,12 @@ const User = require("../models/User.model")
 
 const { isLoggedIn } = require("../middlewares/auth.middlewares.js")
 
+
 router.get("/", isLoggedIn, (req, res, next) => {
     console.log("El usuario hace la solicitud", req.session.activeUser)
     User.findById(req.session.activeUser._id)
     .then((response) => {
-        res.render ("profile/my-profile.hbs", {
+        res.render ("profile/profile.hbs", {
             userDetails: response
         })
     })
@@ -18,7 +19,59 @@ router.get("/", isLoggedIn, (req, res, next) => {
     })
 })
 
-//EDITAR PERFIL
+router.get("/edit-profile", (req, res, next) => {
+    console.log("El usuario hace la solicitud", req.session.activeUser)
+    User.findById(req.session.activeUser._id)
+    .then((response) => {
+        res.render ("profile/edit-profile.hbs", {
+            userDetails: response
+        })
+    })
+    .catch((error) => {
+        next(error)
+    })
+})
+
+// POST recibe el usuario a editar en mongo
+router.post("/:userId/edit", (req, res, next) => {
+
+    // recibir la data a a editar
+    const { userId } = req.params
+    const { username, email, password } = req.body
+  
+    const userToEdit = {
+      username,
+      email,
+      password
+    }
+  
+    User.findByIdAndUpdate(userId, userToEdit)
+    .then(() => {
+      // redireccionar al usuario
+      // res.redirect("/profile")
+      res.redirect(`/profile`)
+    })
+    .catch((error) => {
+      next(error)
+    })
+  
+  
+  })
+
+// Borrar usuario del mongo
+router.post("/:userId/delete", (req, res, next) => {
+
+    // 1. buscar por su id y borrarlo
+    User.findByIdAndDelete(req.params.userId)
+    .then(() => {
+      // 2. redireccionar a "/""
+      res.redirect("/")
+    })
+    .catch((error) => {
+      next(error)
+    })
+  
+  })
 
 
 module.exports = router;
