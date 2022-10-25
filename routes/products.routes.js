3b854951334c36af6b5445c405f13dfd7444a953
johndoe@example.com <johndoe@example.com>
2022-10-25 12:09:06 +0100

    Prodroducts

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,5 +3,21 @@ const express = require("express");
 const router = express.Router();
 
 
+const { isAdmin } = require("../middlewares/auth.middlewares.js")
+
+
+router.get("/", (req, res, next) =>{
+    Products.find()
+    .then((response) =>{
+        res.render("profile/products.hbs", {
+            productDetails: response
+
+        })
+        
+    }) 
+    .catch((error) =>{
+        next(error)
+    })
+})
 
 module.exports = router;
